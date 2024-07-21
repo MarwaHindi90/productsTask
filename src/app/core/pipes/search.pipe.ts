9f667360e1c5b5d479b4f5d9d0554f26cc733bf9
MarwaHindi90 <marwashind@gmail.com>
2024-07-21 17:10:04 +0300

    condition in pipe search

--- src/app/core/pipes/search.pipe.ts
+++ src/app/core/pipes/search.pipe.ts
@@ -7,6 +7,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(products: any[], searchText: string): any {
+    if (!products || !searchText) {
+      return products;
+    }
 
     return products.filter(product => {
       searchText = searchText.toLowerCase();
